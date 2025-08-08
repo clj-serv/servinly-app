7b954fe5e2be05a854b98ad4a983d7f1011f4a31
Claudio Lopes <claudiolopesjr29@gmail.com>
2025-08-08 15:52:03 +1000

    chore: enable React automatic JSX runtime (Option B)

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
-    '@babel/preset-react',
-    '@babel/preset-typescript',
+    [
+      'next/babel',
+      {
+        'preset-react': {
+          runtime: 'automatic',
+          importSource: 'react',
+        },
+      },
+    ],
   ],
 };
